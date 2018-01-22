1d531891b181ffcdada173ad70bf8add3456150e
John M Daly <jmdaly@gmail.com>
2018-01-21 21:17:43 -0500

    Update: Button between space bars changes layers

--- keyboards/dz60/keymaps/jmdaly/keymap.c
+++ keyboards/dz60/keymaps/jmdaly/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, 
 		CTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, 
 		KC_LSPO, __x__, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, MO(_L2), 
-		MO(_L2), KC_LGUI, KC_LALT, KC_ENT, KC_SPC, KC_SPC, KC_RALT, __x__, KC_NO, __x__, KC_RCTL),
+		MO(_L2), KC_LGUI, KC_LALT, KC_ENT, MO(_L2), KC_SPC, KC_RALT, __x__, KC_NO, __x__, KC_RCTL),
 
 	[_L2] = KEYMAP( /* Function layer */
 		KC_TRNS, KC_F1,   KC_F2,   KC_F3, KC_F4, KC_F5, KC_F6,   KC_F7,   KC_F8, KC_F9,    KC_F10,  KC_F11,   KC_F12, KC_TRNS, KC_INS,  \
