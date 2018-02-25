2114f4dc930337ab6d4ba55d0e15521f259930e8
John M Daly <jmdaly@gmail.com>
2018-02-25 14:49:18 -0500

    Setup SpaceFn on bottom row, and add an Enter key left of spacebar

--- keyboards/bananasplit/keymaps/jmdaly/keymap.c
+++ keyboards/bananasplit/keymaps/jmdaly/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,        KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,   KC_Y,    KC_U,       KC_I,       KC_O,       KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, 
 		CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,   KC_F,    KC_G,   KC_H,    KC_J,       KC_K,       KC_L,       KC_SCLN, KC_QUOT, KC_ENT, 
 		KC_LSPO,       __x__,   KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,    KC_N,       KC_M,       KC_COMM,    KC_DOT,  KC_SLSH, KC_RSPC, MO(_L2), 
-		MO(_L2),       KC_LGUI, KC_LALT, KC_SPC, KC_SPC,  KC_SPC, KC_RGUI, LCTL(KC_H), LCTL(KC_J), LCTL(KC_K), LCTL(KC_L)),
+		MO(_L2),       KC_LGUI, ALT_T(KC_ENT), LT(_L2, KC_SPC), LT(_L2, KC_SPC),  LT(_L2, KC_SPC), KC_RGUI, LCTL(KC_H), LCTL(KC_J), LCTL(KC_K), LCTL(KC_L)),
 
 	[_L2] = KEYMAP_ALL( /* Function layer */
 		KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,   KC_F12,  KC_TRNS,  KC_INS,  \
