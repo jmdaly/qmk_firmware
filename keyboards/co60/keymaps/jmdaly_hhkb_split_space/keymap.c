831b8ab60404d1cdb36091bb2e8ecacb3a09da2a
John M Daly <jmdaly@gmail.com>
2018-08-24 21:45:14 -0400

    Add: 6.25u HHKB split space layout

--- keyboards/co60/keymaps/jmdaly_hhkb_split_space/keymap.c
+++ keyboards/co60/keymaps/jmdaly_hhkb_split_space/keymap.c
@@ -23,19 +23,19 @@ enum co60_layers {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_L1] = LAYOUT_60_hhkb_split_space( /* Base */
+  [_L1] = LAYOUT_60_hhkb_split_625u_space( /* Base */
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7, KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,  KC_BSLS, KC_GRV, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC, KC_BSPC, \
     CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,  \
     KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, MO(_L2),   \
-             KC_LGUI, KC_LALT,       KC_ENT,              MO(_L2),       KC_SPC,           KC_RALT, KC_RGUI \
+             KC_LALT, KC_LGUI,       KC_ENT,              MO(_L2),       KC_SPC,  KC_RCTL, KC_RGUI, KC_RALT \
   ),
-  [_L2] = LAYOUT_60_hhkb_split_space( /* Function */
+  [_L2] = LAYOUT_60_hhkb_split_625u_space( /* Function */
     RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,     KC_F12,   _______, _______,   \
-    _______, BL_TOGG, BL_INC,  BL_ON, BL_OFF, _______, _______, _______, _______, _______,   _______,   KC_PGUP,    KC_INSERT,  KC_DEL, \
+    BL_BRTG, BL_TOGG, BL_INC,  BL_DEC, BL_ON, BL_OFF, _______, _______, _______, _______,   _______,   KC_PGUP,    KC_INSERT,  KC_DEL, \
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  KC_HOME,   KC_END,     _______,  \
     _______, BL_DEC,  _______, _______, _______, _______, _______, _______, _______, _______,   KC_PGDOWN, _______, _______,   \
-             _______, _______,          _______,          _______,          _______,            _______,   _______ \
+             _______, _______,          _______,          _______,          _______, _______,   _______,   _______ \
   )
 };
 
