6bdefc856e2f94afc56f60ed40766c91add77ba9
John M Daly <jmdaly@gmail.com>
2018-12-02 12:29:41 -0500

    Update: Enable leader key

--- keyboards/co60/rules.mk
+++ keyboards/co60/rules.mk
@@ -66,6 +66,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+LEADER_ENABLE = yes         # Turn on leader support
 
 # Layouts supported by this PCB:
 LAYOUTS = 60_ansi 60_iso 60_ansi_split_bs_rshift 60_hhkb 60_hhkb_split_space
