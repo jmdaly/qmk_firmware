ca3df631b3f5f0187543faeba1cdb18ade919285
John M Daly <jmdaly@gmail.com>
2018-12-08 23:33:36 -0500

    Fix: Disable unnecessary flags
    CONSOLE_ENABLE appeared to be causing issues with the keyboard on
    my Linux machines. Each time I tried to switch layers, the board
    would hang. Removing CONSOLE_ENABLE appears to fix that.

--- keyboards/handwired/co60/rev6/rules.mk
+++ keyboards/handwired/co60/rev6/rules.mk
@@ -47,12 +47,12 @@ BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
 ## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
 MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
+CONSOLE_ENABLE = no	# Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
 #SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes	    # USB Nkey Rollover
 CUSTOM_MATRIX = yes # Custom matrix file
-AUDIO_ENABLE = yes
+AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = no
 # SERIAL_LINK_ENABLE = yes
 ENCODER_ENABLE = no
