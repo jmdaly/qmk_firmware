f8cb50b9d75a401a4978596333c56724c1aef101
John M Daly <jmdaly@gmail.com>
2019-02-22 21:38:51 -0500

    Update: Switch Rev.6 back to custom matrix
    I'm having an issue with the matrix.c from the qmk
    repo for this rev of the PCB. I don't know why - the Rev.7 version
    works with the general matrix. I hope to sort this out, but for now
    reverting is easiest.

--- keyboards/handwired/co60/rev6/rules.mk
+++ keyboards/handwired/co60/rev6/rules.mk
@@ -38,7 +38,8 @@ OPT_DEFS += -DNO_SUSPEND_POWER_DOWN
 DFU_ARGS = -d 0483:df11 -a 0 -s 0x08000000:leave
 
 # Code for backlight breathing:
-SRC += led.c
+SRC += matrix.c \
+       led.c
 
 # Build Options
 #   comment out to disable the options.
@@ -56,3 +57,4 @@ MIDI_ENABLE = no        # MIDI controls
 UNICODE_ENABLE = no     # Unicode
 BLUETOOTH_ENABLE = no   # Enable Bluetooth with the Adafruit EZ-Key HID
 LEADER_ENABLE = yes
+CUSTOM_MATRIX = yes
